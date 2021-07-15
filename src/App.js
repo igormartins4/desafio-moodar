a685a1e74dacb9d5632dc63d1f9aaa004a0b27fa
Igor Martins <iguinhoigor4@gmail.com>
2021-07-15 14:19:46 -0300

    feat: Criar base da aplicação

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
-import logo from './assets/images/logo.svg';
-
-import { Home } from "./pages/Home";
-import { NewRoom } from "./pages/NewRoom";
-import { Room } from './pages/Room';
-import { AdminRoom } from './pages/AdminRoom';
-
-import { AuthContextProvider } from './contexts/AuthContext';
+import { Table } from "./components/Table/index";
 
 function App() {
   return (
-    
+    <Table />  
   );
 }
 
---
+++ src/components/Table/index.js
@@ -0,0 +1,7 @@
+import './style.scss';
+
+export function Table() {
+    return (
+     <h1>Olá</h1>  
+    )
+}
