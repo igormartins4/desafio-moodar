b1d6b9fb31f9abe762acba197929f5d49ce9ac4c
Igor Martins <iguinhoigor4@gmail.com>
2021-07-17 20:05:57 -0300

    feat: Criar componenten de Checkbox

---
+++ src/components/Checkbox/index.js
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+export function Checkbox(props) {
+  const [checked, setChecked] = useState(false);
+
+  function toggleChecked() {
+    setChecked((prevState) => !checked);
+
+    const checkboxName = props.name.split("-");
+
+    if (
+      checkboxName[0] === "analise" ||
+      checkboxName[0] === "contas" ||
+      checkboxName[0] === "customização" ||
+      checkboxName[0] === "financeiro"
+    ) {
+      if (checked === false) {
+        setChecked(true);
+        return checked === true;
+      } else if (checked === true) {
+        setChecked(false);
+        return checked === false;
+      }
+    }
+  }
+  useEffect(() => {}, [checked]);
+
+  console.log(checked);
+
+  return (
+    <td>
+      <label htmlFor={props.name}>
+        <input
+          type="checkbox"
+          id={props.name}
+          name={props.name}
+          defaultChecked={checked}
+          onChange={toggleChecked}
+        />
+      </label>
+    </td>
+  );
+}
--- src/components/RowTable/index.js
+++ src/components/RowTable/index.js
@@ -1,75 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
+import { Checkbox } from "../Checkbox/index";
 
 import "./style.scss";
 import { formataCaracteres } from "../../utils/formataCaracteres";
 
-
 export function RowTable(props) {
-  const [checked, setChecked] = useState(false);
-
-  const handleOnChange = () => {
-    setChecked((prevState) =>  !prevState);
-    console.log(checked);
-  };
-
-  const columnName1 = formataCaracteres(`${props.row}-ver-listagem`);
-  const columnName2 = formataCaracteres(`${props.row}-ver-detalhes`);
-  const columnName3 = formataCaracteres(`${props.row}-criar`);
-  const columnName4 = formataCaracteres(`${props.row}-editar`);
-  const columnName5 = formataCaracteres(`${props.row}-deletar`);
+  const columnNameListagem = formataCaracteres(`${props.row}-ver-listagem`);
+  const columnNameDetalhes = formataCaracteres(`${props.row}-ver-detalhes`);
+  const columnNameCriar = formataCaracteres(`${props.row}-criar`);
+  const columnNameEditar = formataCaracteres(`${props.row}-editar`);
+  const columnNameDeletar = formataCaracteres(`${props.row}-deletar`);
 
   return (
     <tr className="container">
       <td>
         <span>{props.row}</span>
       </td>
-      <td>
-        <label htmlFor={columnName1}></label>
-      </td>
-      <td>
-        <label htmlFor={columnName2}>
-          <input
-            type="checkbox"
-            id={columnName2}
-            name={columnName2}
-            defaultChecked={checked}
-            onChange={handleOnChange}
-          />
-        </label>
-      </td>
-      <td>
-        <label htmlFor={columnName3}>
-          <input
-            type="checkbox"
-            id={columnName3}
-            name={columnName3}
-            defaultChecked={checked}
-            onChange={handleOnChange}
-          />
-        </label>
-      </td>
-      <td>
-        <label htmlFor={columnName4}>
-          <input
-            type="checkbox"
-            id={columnName4}
-            name={columnName4}
-            defaultChecked={checked}
-            onChange={handleOnChange}
-          />
-        </label>
-      </td>
-      <td>
-        <label htmlFor={columnName5}>
-          <input
-            type="checkbox"
-            id={columnName5}
-            name={columnName5}
-            defaultChecked={checked}
-            onChange={handleOnChange}
-          />
-        </label>
-      </td>
+      <Checkbox name={columnNameListagem} />
+      <Checkbox name={columnNameDetalhes} />
+      <Checkbox name={columnNameCriar} />
+      <Checkbox name={columnNameEditar} />
+      <Checkbox name={columnNameDeletar} />
     </tr>
   );
 }
